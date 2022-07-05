10ecd0d5835b3a2c00b0085d6192fe527119a03d
DarkMaien8117 <maien8117@gmail.com>
2022-07-05 23:27:04 +0900

    complete

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static(`${__dirname}/build`))
+
+app.use('/*', (req, res) => {
+  res.sendFile(`${__dirname}/build/index.html`)
+})
+
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+  console.info(`server started on port ${port}`); // eslint-disable-line no-console
+});
